b08292445cefd5a77cd653ad7b5a05fc712e60c0
Greg Roques <greg@gregroques.com>
2018-09-09 18:25:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ function placeOrder(cardNumber) {
       i++;}
     console.log(`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`);
   }
-   cart = [];
-    return cart;
 }
+ cart = [];
+    return cart;
