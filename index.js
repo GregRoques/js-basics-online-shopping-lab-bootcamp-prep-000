680f4633c63f13746dfaf9718a118f5983fdcb82
Greg Roques <greg@gregroques.com>
2018-09-09 18:01:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function removeFromCart(item) {
  return (`That item is not in your cart.`);
 }
 function placeOrder(cardNumber) {
-  if(cardNumber.length===0){
+  if(cardNumber===0){
     return("Sorry, we don't have a credit card on file for you.");
   }
 }
