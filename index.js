502dbf3344d1bc812dfb03090a0fbc03430de349
Greg Roques <greg@gregroques.com>
2018-09-09 18:41:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ function placeOrder(cardNumber) {
       i++;}
     console.log(`Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`);
     cart.splice(0,cart.length);
+    return cart;
     }
     
 }
